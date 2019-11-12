7285d2a55652acf2b848211ec5539ad2e033a931
Mullen Andrew <andrew.mullen@avast.com>
2019-11-12 13:57:56 +0100

    no staic

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,12 +138,6 @@ TINYMCE_DEFAULT_CONFIG = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
