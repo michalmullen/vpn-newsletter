cdff48936ee717dcba46659d922fa923d697ce0d
Mullen Andrew <andrew.mullen@avast.com>
2019-11-12 15:22:19 +0100

    security update

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6@z^gmhn&4uhmggsnyi_5d%!_l#f1(grd&*uqk&k(=+%t7*k$7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -150,8 +150,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'emailbotcz@gmail.com'
-EMAIL_HOST_PASSWORD = 'IR7AHv5yZVd5wtctBT'
-
+#EMAIL_HOST_PASSWORD = 'IR7AHv5yZVd5wtctBT'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 # heroku db setup
 
--- users/forms.py
+++ users/forms.py
@@ -18,9 +18,9 @@ class UserRegisterForm(UserCreationForm):
     def clean_email(self):
         # Get the email
         email = self.cleaned_data.get('email')
-        # if email.split('@')[1] != "avast.com":
-        #     raise forms.ValidationError(
-        #         'To register you must use an Avast account.')
+        if email.split('@')[1] != "avast.com":
+            raise forms.ValidationError(
+                'To register you must use an Avast account.')
 
         # Check to see if any users already exist with this email as a username.
         try:
